dabe33e71fa68db0178a41eb517cf0c16878bf6f
KmySamayoa <camila.samayoa@gmail.com>
2014-07-24 20:23:53 +0000

    generated controllers with rails

--- app/models/blog.rb
+++
@@ -1,9 +0,0 @@
-class Blog < ActiveRecord::Base
-belongs_to :user
-has_many :blog_categories
-has_many :categories, through: :blog_categories
-
-validates :title, presence: true
-validates :content, presence: true
-validates :categories, presence: true
-end
--- app/models/blog_category.rb
+++
@@ -1,4 +0,0 @@
-class BlogCategory < ActiveRecord::Base
-  belongs_to :blog 
-  belongs_to :category
-end
